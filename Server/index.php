10e2dbfc2167284c68da9b49ecf44617190ff7ed
Thomas K <thomas.koscheck@protonmail.ch>
2018-05-08 23:15:28 +0200

    New android layout, hashsum implementation

--- Server/index.php
+++ Server/index.php
@@ -1,48 +1,54 @@
 <?php
-
-//Connect to MariaDB
-$dbhost = 'localhost';
-$dbuser = 'database-user';
-$dbname = 'database-name';
-$dbpass = 'database-user-password';
-$dbtable = 'database-table';
-
-$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-if ($conn->connect_errno) {
-    die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
-}
-
+//getting the values from app
 $product = $_GET['product'];
 $requester = $_GET['requester'];
 $price = $_GET['price'];
+$password = $_GET['password'];
 
-//Einzelne Abfragen
-$sql_getCredit = "SELECT credit FROM $dbtable ORDER BY id DESC LIMIT 1";
+//get login credentials from outside of webroot
+$constants = parse_ini_file("path-to-ini-file-outside-of-webroot");
+$dbpass = $constants['database-user-password'];
+$auth = $constants['auth-code'];
 
-//Vorbereitung zu Abfrage
-$retval = $conn->prepare($sql_getCredit);
-$retval->execute();
+if (hash('sha256', $password) === $auth) {
+    //Connect to MariaDB
+    $dbhost = 'localhost';
+    $dbuser = 'database-user';
+    $dbname = 'database-name';
+    $dbpass = 'database-user-password';
+    $dbtable = 'database-table';
 
-//Holen und Verarbeiten der Ergebnisse
-$result = $retval->get_result();
-while($row = $result->fetch_assoc())
-{
-    $lastCredit = $row['credit'];
-}
+    $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+    if ($conn->connect_errno) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
 
-//neues Preis berechnen
-$credit = $lastCredit-$price;
+    //single query
+    $sql_getCredit = "SELECT credit FROM $dbtable ORDER BY id DESC LIMIT 1";
 
-//INSERT INTO DATABASE
-$sql_setNew = "INSERT INTO $dbtable (credit, product, requester, price)
+    //preparation for query
+    $retval = $conn->prepare($sql_getCredit);
+    $retval->execute();
+
+    //getting and processing the results
+    $result = $retval->get_result();
+    while ($row = $result->fetch_assoc()) {
+        $lastCredit = $row['credit'];
+    }
+
+    //calculate new credit-limit
+    $credit = $lastCredit - $price;
+
+    //INSERT INTO DATABASE
+    $sql_setNew = "INSERT INTO $dbtable (credit, product, requester, price)
                 VALUES ('$credit', '$product', '$requester', '$price')";
 
+    if ($conn->query($sql_setNew) === false) {
+        echo "Error: " . $sql_setNew . "<br>" . $conn->error;
+    }
 
-if ($conn->query($sql_setNew) === FALSE) {
-    echo "Error: " . $sql_setNew . "<br>" . $conn->error;
-}
+    $conn->close();
 
-$conn->close();	
-
-echo $credit;
-?>
+    echo $credit;
+} else {
+    echo "Your IP is logged. The the responsible authorities are informed.";}
