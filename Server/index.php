f19abeddc0deb5fe5d43c5e167d90c6ebb15fa39
Thomas K <thomas.koscheck@protonmail.ch>
2018-05-10 11:05:21 +0200

    echoing json file on serverside
    moving sample .json
    adding sample .ini file

--- Server/index.php
+++ Server/index.php
@@ -11,7 +11,9 @@
     $constants = parse_ini_file("path-to-ini-file-outside-of-webroot");
     $dbpass = $constants['database-user-password'];
     $auth = $constants['auth-code'];
-     
+
+    $appVersion = $constants['app_version'];
+  
     $challenge = bin2hex(random_bytes(200));
      
     if (hash('sha256', $password) === $auth) {
@@ -19,7 +21,6 @@
         $dbhost = 'localhost';
         $dbuser = 'database-user';
         $dbname = 'database-name';
-        $dbpass = 'database-user-password';
         $dbtable = 'database-table';
      
         $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
@@ -31,32 +32,64 @@
         $sql_getCredit = "SELECT credit FROM $dbtable ORDER BY id DESC LIMIT 1";
      
         //preparation for query
-        $retval = $conn->prepare($sql_getCredit);
-        $retval->execute();
-     
+        $retvalCredit = $conn->prepare($sql_getCredit);
+        $retvalCredit->execute();
+        
         //getting and processing the results
-        $result = $retval->get_result();
-        while ($row = $result->fetch_assoc()) {
+        $resultCredit = $retvalCredit->get_result();
+        while ($row = $resultCredit->fetch_assoc()) {
             $lastCredit = $row['credit'];
         }
      
-        //calculate new credit-limit
+        //calculate new credit-limit and date
         $credit = $lastCredit - $price;
+        $date = date('Y-m-d');
      
         //INSERT INTO DATABASE
-        $sql_setNew = "INSERT INTO $dbtable (credit, product, requester, price)
-            VALUES ('$credit', '$product', '$requester', '$price')";
-     
+        $sql_setNew = "INSERT INTO $dbtable (credit, product, requester, price, date, done)
+                    VALUES ('$credit', '$product', '$requester', '$price', '$date', '0')";
+
         if ($conn->query($sql_setNew) === false) {
             echo "Error: " . $sql_setNew . "<br>" . $conn->error;
         }
      
+        //single query
+        $sql_getUndone = "SELECT product, requester, price, date FROM $dbtable WHERE done=0";
+
+        //getting and processing the results      
+        if ( $retvalUndone = $conn->query($sql_getUndone)) {
+            $resultString = "";
+            /* fetch associative array */
+            while ($row = $retvalUndone->fetch_assoc()) {
+                $resultString .= '{ "requester":"' . $row["requester"] . '",' .
+                    '"product":"' . $row["product"] . '",' . 
+                    '"price":"' . $row['price'] . '",' .
+                    '"date":"' . $row['date'] . '",' .
+                    "},";
+            } 
+
+            /* free result set */
+            $retvalUndone->free();
+        }
+        
         $conn->close();
-     
-        echo $credit;
+
+        buildJSON($credit, $appVersion, $resultString);
      
     } else {echo "Your IP is logged. The the responsible authorities are informed.";} //else wrong password
         
+}
+
+function buildJSON($credit, $appVersion, $resultString)
+{
+
+    $return = "{" .
+        '"credit":"' . $credit . '",' .
+        '"newestAppVersion":"' . $appVersion . '",' .
+        '"expenses":[' . $resultString .
+        "]}";
+
+    echo $return;
 }
     
 function sqlFakeAction($password) {  
@@ -82,25 +115,44 @@ function sqlFakeAction($password) {
     
         //single query
         $sql_getCredit = "SELECT credit FROM $dbtable ORDER BY id DESC LIMIT 1";
-    
+
         //preparation for query
-        $retval = $conn->prepare($sql_getCredit);
-        $retval->execute();
-    
+        $retvalCredit = $conn->prepare($sql_getCredit);
+        $retvalCredit->execute();
+
         //getting and processing the results
-        $result = $retval->get_result();
-        while ($row = $result->fetch_assoc()) {
+        $resultCredit = $retvalCredit->get_result();
+        while ($row = $resultCredit->fetch_assoc()) {
             $lastCredit = $row['credit'];
         }
-    
+
         //calculate new credit-limit
         $credit = $lastCredit - $price;
-    
+
+        //single query
+        $sql_getUndone = "SELECT product, requester, price, date FROM $dbtable WHERE done=0";
+
+        //getting and processing the results      
+        if ($retvalUndone = $conn->query($sql_getUndone)) {
+            $resultString = "";
+            /* fetch associative array */
+            while ($row = $retvalUndone->fetch_assoc()) {
+                $resultString .= '{ "requester":"' . $row["requester"] . '",' .
+                    '"product":"' . $row["product"] . '",' . 
+                    '"price":"' . $row['price'] . '",' .
+                    '"date":"' . $row['date'] . '",' .
+                    "},";
+            } 
+
+            /* free result set */
+            $retvalUndone->free();
+        }
+        
         $conn->close();
-        echo $credit;
+
+        buildJSON($credit, $appVersion, $resultString);
     
-    } else {echo "Your IP is logged. The the responsible authorities are informed.";} //else wrong password
-       
+    } else {echo "Your IP is logged. The the responsible authorities are informed.";} //else wrong password     
 }
     
 
