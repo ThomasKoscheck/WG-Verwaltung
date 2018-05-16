1f38a6814efe3d23302882df8e51183efe8802d1
Mario <jajakob@gmx.de>
2018-05-16 17:07:13 +0200

    Added currency symbol to server side json string

--- Server/jsonHandler.py
+++ Server/jsonHandler.py
@@ -55,7 +55,7 @@ def buildJSON():
         for row in results:
             jsonstring += '{ "requester":"' + str(row[0]) + '",' + \
                     '"product":"' + str(row[1]) + '",' + \
-                    '"price":"' + str(row[2]) + '",' +\
+                    '"price":"' + str(row[2]) + ' €",' +\
                     '"date":"' + str(row[3]) + '"' + \
                     "},"
 
