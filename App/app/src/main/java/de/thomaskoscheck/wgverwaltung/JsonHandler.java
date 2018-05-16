8a82e1a6f48c3d11c0ab95308685c232f19972fc
Mario <jajakob@gmx.de>
2018-05-16 16:55:44 +0200

    Changed hardcoded strings to hardcoded final static variables

--- App/app/src/main/java/de/thomaskoscheck/wgverwaltung/JsonHandler.java
+++ App/app/src/main/java/de/thomaskoscheck/wgverwaltung/JsonHandler.java
@@ -5,20 +5,28 @@ import org.json.*;
 import de.thomaskoscheck.wgverwaltung.ServerCommunication.Expense;
 import de.thomaskoscheck.wgverwaltung.ServerCommunication.SendDetails;
 import de.thomaskoscheck.wgverwaltung.ServerCommunication.ServerResponse;
+import de.thomaskoscheck.wgverwaltung.Setting.SettingsActivity;
 
 public class JsonHandler {
 
+    private static final String CREDIT ="credit";
+    private static final String EXPENSES ="expenses";
+    private static final String REQUESTER ="requester";
+    private static final String NEWESTAPPVERSION ="newestAppVersion";
+    private static final String PRODUCT ="product";
+    private static final String PRICE ="price";
+
     public static ServerResponse parseJson(String json) throws JSONException {
         JSONObject response = new JSONObject(json);
-        String credit = response.getString("credit");
-        String newestAppVersion = response.getString("newestAppVersion");
-        JSONArray expensesJson = response.getJSONArray("expenses");
+        String credit = response.getString(CREDIT);
+        String newestAppVersion = response.getString(NEWESTAPPVERSION);
+        JSONArray expensesJson = response.getJSONArray(EXPENSES);
         Expense[] expenses = new Expense[expensesJson.length()];
 
         for (int i = 0; i < expensesJson.length(); i++) {
-            String requester = expensesJson.getJSONObject(i).getString("requester");
-            String product = expensesJson.getJSONObject(i).getString("product");
-            String priceString = expensesJson.getJSONObject(i).getString("price");
+            String requester = expensesJson.getJSONObject(i).getString(REQUESTER);
+            String product = expensesJson.getJSONObject(i).getString(PRODUCT);
+            String priceString = expensesJson.getJSONObject(i).getString(PRICE);
             double price = Double.parseDouble(priceString);
             expenses[i] = new Expense(requester, product, price);
         }
@@ -28,9 +36,9 @@ public class JsonHandler {
     public static String generateJsonString(SendDetails sendDetails) {
         JSONObject jsonRequest = new JSONObject();
         try {
-            jsonRequest.put("requester", sendDetails.getSettings().getRequester());
-            jsonRequest.put("price", sendDetails.getRequestDetails().getPrice());
-            jsonRequest.put("product", sendDetails.getRequestDetails().getProduct());
+            jsonRequest.put(REQUESTER, sendDetails.getSettings().getRequester());
+            jsonRequest.put(PRICE, sendDetails.getRequestDetails().getPrice());
+            jsonRequest.put(PRODUCT, sendDetails.getRequestDetails().getProduct());
             return jsonRequest.toString();
         } catch (JSONException e) {
             e.printStackTrace();
