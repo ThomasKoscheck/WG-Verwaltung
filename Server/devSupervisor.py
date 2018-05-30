6b8f6a910217127ddd3c7750487055accf78a320
Thomas K <thomas.koscheck@protonmail.ch>
2018-05-30 18:08:19 +0200

    fixed some errors and loop statement

---
+++ Server/devSupervisor.py
@@ -0,0 +1,110 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*-
+
+import os
+from subprocess import Popen
+import sendMail
+from datetime import date
+
+PATH = '/path/to/your/server/folder'
+PIDFILE = PATH + "/pid.txt"
+
+def readFile(filePath):
+    try:
+        file = open(filePath, "r") 
+        data = file.read() 
+        file.close() 
+        return data
+    except Exception as e:
+        print(e)
+
+def writeFile(filePath, content):
+    try:
+        file = open(filePath, "w")
+        file.write(str(content))
+        file.close()
+    except Exception as e:
+        print(e)
+
+def startServer():
+    try:
+        pathToFile = PATH + '/development/server.py'
+        proc = Popen(['python', pathToFile, '9998 &'], close_fds=True)
+        pid = proc.pid # access `pid` attribute to get the pid of the child process.
+        return pid
+
+    except Exception as e:
+        print(str(e))
+        sendMail.sendError("Error starting server on development branch", str(e) + "\nTime: "  + str(date.today()))
+        return -1
+
+def killOldServer():  
+    try:
+        # read pid in file
+        # I do nor reuse readFile(), because I dont want exception handling 
+        pidFile = open(PIDFILE, "r") 
+        oldPid = pidFile.read() 
+        pidFile.close() 
+
+        # kill runnng server
+        killStatement = 'kill ' +  str(oldPid)
+        os.system(killStatement)
+
+    # always breaks on first run, because no old devServer exists
+    except Exception as e:
+	    print(e)
+        pass
+
+
+def cloneRepo():
+    try:
+        os.system('rm -rf ' + PATH + '/development/')
+        
+        # clone current branch
+        git_command = 'git clone --branch --quiet server-development https://github.com/ThomasKoscheck/WG-Verwaltung.git ' + PATH + '/WG-Verwaltung/'
+        os.system(git_command)
+        
+        # move and cleanup code
+        os.system('mkdir -p ' + PATH + '/development/')
+        mv_command = 'mv ' + PATH + '/WG-Verwaltung/Server/* ' + PATH +'/development/'
+        os.system(mv_command)
+        os.system('rm -rf ' + PATH + '/WG-Verwaltung/')
+
+    except Exception as e:
+        print(e)
+
+def manipulateFiles():
+    try:
+        # exchange path to config.ini in credentials.py
+        filedata_credentials = readFile(PATH + '/development/credentials.py')
+        filedata_credentials = filedata_credentials.replace('/path-to-config-ini-file', '/path/to/your/config/file')
+        writeFile(PATH +"/development/credentials.py", filedata_credentials)
+
+        # exchange database calling no.1
+        filedate_sqlHandler = readFile(PATH + '/development/sqlHandler.py')
+        filedate_sqlHandler = filedate_sqlHandler.replace('credentials.getDBTABLE()', 'credentials.getDBTABLEDEV()')
+        writeFile(PATH + '/development/sqlHandler.py', filedate_sqlHandler)  
+        # exchange database calling no.2
+        filedate_sqlHandler = readFile(PATH + '/development/jsonHandler.py')
+        filedate_sqlHandler = filedate_sqlHandler.replace('credentials.getDBTABLE()', 'credentials.getDBTABLEDEV()')
+        writeFile(PATH + '/development/jsonHandler.py', filedate_sqlHandler)  
+
+    except Exception as e:
+        print(e)
+          
+
+
+# get newest code
+cloneRepo()
+
+# kill old Server
+killOldServer()
+
+# change values to dev values
+manipulateFiles()
+
+# try to start new server
+newPid = startServer()
+
+# write pid in file for later use
+writeFile(PIDFILE, newPid)
--- Server/server.py
+++ Server/server.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import socket
+import sys
 
 from time import sleep
 
@@ -10,48 +11,58 @@ import bcolors
 import credentials
 import jsonHandler
 import sqlHandler
+import sendMail
 
 AES_BLOCK_SIZE = 16
-    
+
+# pass port as argument
+try:
+    sys.argv[1]
+
+except Exception as e:
+    print(bcolors.color.FAIL + "--- You have to pass a port as argument (python server.py 9999) ---\n" + bcolors.color.ENDC + "\n")
+    sys.exit() 
+
+# uncomment print lines for better debugging    
 def listen():
-    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    connection.bind(('0.0.0.0', 9999))
-    connection.listen(10)
-    while True:
-        current_connection, address = connection.accept()
-        while True:            
-            # generate IV for AES encryption
-           
-            IV = aescbc.generateIV(AES_BLOCK_SIZE) 
-            current_connection.send(IV)
+    try:  
+        connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        connection.bind(('0.0.0.0', int(sys.argv[1])))
+        connection.listen(10)
 
-            IV = IV[:16] # because of some strange things 
-                     
-            print(bcolors.color.HEADER + "--- Generated IV for AES ---\n" + bcolors.color.ENDC + IV + "\n") 
+        while True:
+            current_connection, address = connection.accept()
+            while True:            
+                # generate IV for AES encryption
+            
+                IV = aescbc.generateIV(AES_BLOCK_SIZE) 
+                current_connection.send(IV)
 
-            datalength = current_connection.recv(6)
-            print(bcolors.color.HEADER + "--- Now receiving json of this datalength ---\n" + bcolors.color.ENDC + datalength + "\n") 
+                IV = IV[:16] # because of some strange things 
+                        
+                #print(bcolors.color.HEADER + "--- Generated IV for AES ---\n" + bcolors.color.ENDC + IV + "\n") 
 
-            try:   
+                datalength = current_connection.recv(6)
+                # print(bcolors.color.HEADER + "--- Now receiving json of this datalength ---\n" + bcolors.color.ENDC + datalength + "\n") 
+                  
                 # getting value of the stream that will be send from client           
                 data = current_connection.recv(int(datalength))
 
-                print(bcolors.color.OKBLUE + "--- Encryted string ---\n" + bcolors.color.ENDC  + data + "\n")                
+                #print(bcolors.color.OKBLUE + "--- Encryted string ---\n" + bcolors.color.ENDC  + data + "\n")                
                 
                 # decrypting the data
                 auth = credentials.getAUTH()
                 data = aescbc.decrypt(data, auth, IV)
 
-                print(bcolors.color.OKBLUE + "--- Got this data ---\n" + bcolors.color.ENDC  + data + "\n")
+                #print(bcolors.color.OKBLUE + "--- Got this data ---\n" + bcolors.color.ENDC  + data + "\n")
 
                 if data.startswith("getServerData"):
-                    
                     json = jsonHandler.buildJSON()
                     jsonpad = aescbc.generatePadding(json)
                     jsonencrypt = aescbc.encrypt(jsonpad, auth, IV)
                     current_connection.send(jsonencrypt)
-                    print(bcolors.color.OKGREEN + "--- Sent this encrypted data to server ---\n" + bcolors.color.ENDC)
+                    #print(bcolors.color.OKGREEN + "--- Sent this encrypted data to server ---\n" + bcolors.color.ENDC)
 
                     current_connection.shutdown(1)
                     current_connection.close() 
@@ -62,25 +73,32 @@ def listen():
                 else:
                     print(bcolors.color.OKBLUE + "--- Client sent this data ---\n" + bcolors.color.ENDC + data + "\n")
 
-                    product, requester, price, dates, done = jsonHandler.parseJSON(data) # getting parsed data from json send from app
+                    sqlId, product, requester, price, dates, done = jsonHandler.parseJSON(data) # getting parsed data from json send from app
                     credit = sqlHandler.getCreditSQL() # get the actual credit
                     credit -= price # new credit
-                    insertIntoSQL(credit, product, requester, price, dates, done)   # inserting date into SQL database
+
+                    if done is 0:
+                        resultState = sqlHandler.insertIntoSQL(credit, product, requester, price, dates, done)   # inserting date into SQL database
+                        if resultState == 1:   # send Email about entry only in case of success 
+                            sendMail.send(credit, product, requester, price, dates, done) 
+
+                    elif done is 1:
+                        sqlHandler.updateSQL(sqlId)   # updating entry to done in SQL database
 
                     current_connection.shutdown(1)
                     current_connection.close()
                     print(bcolors.color.WARNING + "--- Closed connection ---\n" + bcolors.color.ENDC)
                     break
+        sleep(1)
 
-            except Exception as e:    
-                print(bcolors.color.FAIL + str(e) + bcolors.color.ENDC)
-                print(bcolors.color.FAIL + "Either the data is empty or an other error occured getting data" + bcolors.color.ENDC)
-                current_connection.shutdown(1)
-                current_connection.close()
-                break
+
+    except Exception as e:    
+        print(bcolors.color.FAIL + str(e) + bcolors.color.ENDC)
+        print(bcolors.color.FAIL + "Either the data is empty or an other error occured getting data" + bcolors.color.ENDC)
+        current_connection.shutdown(1)
+        current_connection.close()
           
-            sleep(1)
-
+    
 if __name__ == "__main__":
     try:
         print(bcolors.color.OKGREEN + "--- Starting Server ---\n" + bcolors.color.ENDC)
