c31831dbde4df9e017e6cc3705a2da4b66e582b2
Thomas K <thomas.koscheck@protonmail.ch>
2018-09-23 15:19:31 +0200

    Update sendMail.py

--- Server/sendMail.py
+++ Server/sendMail.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.multipart import MIMEText
+from email.MIMEMultipart import MIMEMultipart	
+from email.MIMEText import MIMEText 
 import bcolors 
 import credentials
  
